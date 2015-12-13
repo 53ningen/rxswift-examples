6b68bac0bfa689d579ae9d6d728136db931dbf45
53nigen <korun24276@gmail.com>
2015-12-14 03:33:34 +0900

    add TextFieldSample

---
+++ rxswift-examples/UI/TextFieldSample/TextFieldSampleViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TextFieldSampleViewController.swift
+//  rxswift-examples
+//
+//  Copyright © 2015年 53ningen. All rights reserved.
+//
+
+import RxSwift
+import UIKit
+
+class TextFieldSampleViewController: UIViewController {
+    
+    private var disposeBag: DisposeBag = DisposeBag()
+    @IBOutlet weak var textField: UITextField!
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.rx_text
+            //.filter { $0 == "ココア" }
+            .map { "はぁ... " + $0 + "さん..."}
+            .subscribeNext { NSLog($0) }
+            .addDisposableTo(disposeBag)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        disposeBag = DisposeBag()
+    }
+
+
+}
