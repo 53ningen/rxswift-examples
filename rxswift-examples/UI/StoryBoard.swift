3d1f540ad7efde1a69fa1c0984c66f41498e6703
53nigen <korun24276@gmail.com>
2015-12-14 02:45:53 +0900

    add ButtonSample

---
+++ rxswift-examples/UI/StoryBoard.swift
@@ -0,0 +1,20 @@
+//
+//  StoryBoard.swift
+//  rxswift-examples
+//
+//  Copyright © 2015年 53ningen. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+internal enum Storyboard: String {
+    
+    case ButtonSample = "ButtonSample"
+    
+    internal static func getViewController<T: UIViewController>(storyboardId: Storyboard) -> T {
+        let storyBoard = UIStoryboard(name: storyboardId.rawValue, bundle: NSBundle.mainBundle())
+        return storyBoard.instantiateInitialViewController() as! T
+    }
+    
+}
