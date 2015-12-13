35ee1d995ba2bd283e68f310d8ebf7a3d815452c
53nigen <korun24276@gmail.com>
2015-12-13 23:37:43 +0900

    update rxswift version

--- rxswift-examples/Infrastructure/Http/RxHttpClient.swift
+++ rxswift-examples/Infrastructure/Http/RxHttpClient.swift
@@ -6,14 +6,14 @@ public typealias RequestHeader = (key: String, value: String)
 
 public protocol RxHttpClient {
     
-    func get(url: NSURL, parameters: [RequestParameter]?, headers: [RequestHeader]?, timeoutSec: NSTimeInterval?) -> Observable<(NSData!, NSHTTPURLResponse)>
+    func get(url: NSURL, parameters: [RequestParameter]?, headers: [RequestHeader]?, timeoutSec: NSTimeInterval?) -> Observable<(NSData, NSHTTPURLResponse)>
     
-    func post(url: NSURL, parameters: [RequestParameter]?, headers: [RequestHeader]?, timeoutSec: NSTimeInterval?) -> Observable<(NSData!, NSHTTPURLResponse)>
+    func post(url: NSURL, parameters: [RequestParameter]?, headers: [RequestHeader]?, timeoutSec: NSTimeInterval?) -> Observable<(NSData, NSHTTPURLResponse)>
     
-    func put(url: NSURL, parameters: [RequestParameter]?, headers: [RequestHeader]?, timeoutSec: NSTimeInterval?) -> Observable<(NSData!, NSHTTPURLResponse)>
+    func put(url: NSURL, parameters: [RequestParameter]?, headers: [RequestHeader]?, timeoutSec: NSTimeInterval?) -> Observable<(NSData, NSHTTPURLResponse)>
     
-    func delete(url: NSURL, parameters: [RequestParameter]?, headers: [RequestHeader]?, timeoutSec: NSTimeInterval?) -> Observable<(NSData!, NSHTTPURLResponse)>
+    func delete(url: NSURL, parameters: [RequestParameter]?, headers: [RequestHeader]?, timeoutSec: NSTimeInterval?) -> Observable<(NSData, NSHTTPURLResponse)>
     
-    func execute(method: HttpMethod, url: NSURL, parameters: [RequestParameter]?, headers: [RequestHeader]?, timeoutSec: NSTimeInterval?) -> Observable<(NSData!, NSHTTPURLResponse)>
+    func execute(method: HttpMethod, url: NSURL, parameters: [RequestParameter]?, headers: [RequestHeader]?, timeoutSec: NSTimeInterval?) -> Observable<(NSData, NSHTTPURLResponse)>
     
 }
